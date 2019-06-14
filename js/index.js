8b08812ebb6488667b183f8c1bb9a9fedf2f58c6
Jakub <jakub.sobczynski188@gmail.com>
2019-06-14 16:42:13 +0200

    navbar fixed
    navbar fixed

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@
 var navOpenBtn = document.querySelector('.nav-open-btn');
 var navCloseBtn = document.querySelector('.nav__close');
 var nav = document.querySelector('.nav');
-var pageContent = document.querySelector('.page__content');
+var pageContent = document.querySelectorAll('.page__content');
+var pageContent = [...pageContent]
 var navList = document.querySelector('.nav__list');
 var page = document.querySelector('.page');
 
@@ -13,17 +14,21 @@ navOpenBtn.addEventListener('click', function() {
   navOpenBtn.classList.add('js-hidden');
   
   nav.classList.add('js-opened');
+  for (klass of pageContent) {
+    klass.classList.add('js-opened');
+  }
   
-  pageContent.classList.add('js-opened');
 });
 
 //close nav
 navCloseBtn.addEventListener('click', function() {
   navOpenBtn.classList.remove('js-hidden');
-  
+
   nav.classList.remove('js-opened');
+  for (klass of pageContent) {
+    klass.classList.remove('js-opened');
+  }
   
-  pageContent.classList.remove('js-opened');
 });
 
 //closing navigation if click outside it
@@ -43,46 +48,47 @@ page.addEventListener('click', function(e) {
   
 });
 
-//DEMO CODE - !!!YOU DON'T NEED IT
-var demoBtns = document.querySelectorAll('.demo-btn');
 
 //adding default demo classes
 nav.classList.add('nav--offcanvas-3');
-pageContent.classList.add('page__content--offcanvas-3');
+
+for (klass of pageContent) {
+  klass.classList.add('page__content--offcanvas-3');
+}
 
 //change demo on btn click
-demoBtns.forEach(function(el, ind) {
+// demoBtns.forEach(function(el, ind) {
   
-  el.addEventListener('click', function() {
+//   el.addEventListener('click', function() {
     
-    //add active class to active btn
-    demoBtns.forEach(function(el) {
-      el.classList.remove('js-active');
-    });
+//     //add active class to active btn
+//     demoBtns.forEach(function(el) {
+//       el.classList.remove('js-active');
+//     });
     
-    this.classList.add('js-active');
+//     this.classList.add('js-active');
     
-    //add proper classes for effects
-    var effClass = ind + 1;
+//     //add proper classes for effects
+//     var effClass = ind + 1;
     
-    nav.classList.remove('js-opened');
+//     nav.classList.remove('js-opened');
   
-    pageContent.classList.remove('js-opened');
+//     pageContent.classList.remove('js-opened');
     
-    navCloseBtn.classList.remove('js-opened');
+//     navCloseBtn.classList.remove('js-opened');
   
-    navList.classList.remove('js-opened');
+//     navList.classList.remove('js-opened');
     
-    navOpenBtn.classList.remove('js-hidden');
+//     navOpenBtn.classList.remove('js-hidden');
     
-    nav.classList.remove(nav.classList[1]);
+//     nav.classList.remove(nav.classList[1]);
     
-    nav.classList.add('nav--offcanvas-' + effClass);
+//     nav.classList.add('nav--offcanvas-' + effClass);
     
-    pageContent.classList.remove(pageContent.classList[1]);
+//     pageContent.classList.remove(pageContent.classList[1]);
     
-    pageContent.classList.add('page__content--offcanvas-' + effClass);
+//     pageContent.classList.add('page__content--offcanvas-' + effClass);
     
-  });
+//   });
   
-});
+// });

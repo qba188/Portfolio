5f17fd8242ff6496f7e040e00618a597b0b93e0f
Jakub <jakub.sobczynski188@gmail.com>
2019-06-17 16:33:32 +0200

    nav fixed, add 2 sections

--- js/index.js
+++ js/index.js
@@ -43,7 +43,9 @@ page.addEventListener('click', function(e) {
   
     nav.classList.remove('js-opened');
   
-    pageContent.classList.remove('js-opened');
+    for (klass of pageContent) {
+      klass.classList.remove('js-opened');
+    }
   }
   
 });
@@ -56,39 +58,87 @@ for (klass of pageContent) {
   klass.classList.add('page__content--offcanvas-3');
 }
 
-//change demo on btn click
-// demoBtns.forEach(function(el, ind) {
-  
-//   el.addEventListener('click', function() {
-    
-//     //add active class to active btn
-//     demoBtns.forEach(function(el) {
-//       el.classList.remove('js-active');
-//     });
-    
-//     this.classList.add('js-active');
-    
-//     //add proper classes for effects
-//     var effClass = ind + 1;
-    
-//     nav.classList.remove('js-opened');
-  
-//     pageContent.classList.remove('js-opened');
-    
-//     navCloseBtn.classList.remove('js-opened');
-  
-//     navList.classList.remove('js-opened');
-    
-//     navOpenBtn.classList.remove('js-hidden');
-    
-//     nav.classList.remove(nav.classList[1]);
-    
-//     nav.classList.add('nav--offcanvas-' + effClass);
-    
-//     pageContent.classList.remove(pageContent.classList[1]);
-    
-//     pageContent.classList.add('page__content--offcanvas-' + effClass);
-    
-//   });
-  
-// });
+//TOP PAGE ANIMATIONS//
+const slidesItems = Array.from(document.querySelectorAll('.slides > .slide'));
+			const imgs = {even:[],odd:[]};
+			slidesItems.forEach((item) => {
+				imgs.even.push(item.dataset.imageEven);
+				imgs.odd.push(item.dataset.imageOdd);
+			});
+
+			const dil = new DualImageLayout(document.querySelector('.slices'), {
+				images: {even: imgs.even[0],odd: imgs.odd[0]},
+				slices: 179,
+				orientation: 'horizontal'
+			});
+
+			imagesLoaded(document.body, { background: true }, () => {	
+				animateGridItems();
+            });
+
+			let isAnimating = true;
+			let currentSlide = 0;
+			const slidesNavItems = Array.from(document.querySelectorAll('.slides__nav > .slides__nav-item'));
+			const changeLocation = (ev,pos) => {
+				ev.preventDefault();
+				if ( pos === currentSlide || isAnimating ) {
+					return false;
+				}
+
+				ev.target.classList.add('slides__nav-item--current');
+				slidesNavItems[currentSlide].classList.remove('slides__nav-item--current');
+				
+				animateGridItems(currentSlide,pos);
+				currentSlide = pos;
+				dil.switchImages(imgs.even[currentSlide],imgs.odd[currentSlide],true);
+			};
+			slidesNavItems.forEach((item, pos) => item.addEventListener('click', (ev) => changeLocation(ev,pos)));
+
+			const gridItemsLetters = [];
+			slidesItems.forEach((item) => {
+				Array.from(item.querySelectorAll('.grid__item')).forEach((gridItem) => charming(gridItem));
+				gridItemsLetters.push(Array.from(item.querySelectorAll('span')));
+			});
+
+			const animateGridItems = (currentSlide = -1, pos = 0) => {
+				isAnimating = true;
+				const newLettersFn = () => {
+					const promises = [];
+					for (lettersGroup of gridItemsLetters[pos]) {
+						promises.push(anime({
+							targets: lettersGroup,
+							duration: 10,
+							easing: 'linear',
+							delay: (t,i) => anime.random(200,1000),
+							opacity: [0,1],
+							before: () => lettersGroup.style.opacity = 0
+						}).finished);
+					}
+					Promise.all(promises).then(()=> isAnimating = false);
+				};
+
+				if ( currentSlide !== -1 ) {
+					const promises = [];
+					for (lettersGroup of gridItemsLetters[currentSlide]) {
+						promises.push(anime({
+							targets: lettersGroup,
+							duration: 10,
+							easing: 'linear',
+							delay: (t,i) => anime.random(200,1000),
+							opacity: 0,
+							before: () => lettersGroup.style.opacity = 1
+						}).finished);
+					}
+					Promise.all(promises).then(()=> newLettersFn());
+				}
+				else {
+					newLettersFn();
+				}
+			};
+
+			const glitchText = document.querySelector('.date');
+			charming(glitchText);
+			Array.from(glitchText.querySelectorAll('span')).forEach(letter => {
+				letter.style.opacity = 1;
+				new GlitchFx(letter).glitch();
+			});
